7cd6d17c00a273f683632a82ea418e05ed3c55aa
eyesee <icaonapo12345@gmail.com>
2023-02-22 01:48:38 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,23 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php 
+/* 
+ * PayPal and database configuration 
+ */ 
+  
+// PayPal configuration 
+define('PAYPAL_ID', 'sb-zmmc473913190@business.example.com'); 
+define('PAYPAL_SANDBOX', TRUE); //TRUE or FALSE 
+ 
+define('PAYPAL_RETURN_URL', 'http://localhost/paypaltask/success.php'); 
+define('PAYPAL_CANCEL_URL', 'http://localhost/paypaltask/cancel.php'); 
+define('PAYPAL_CURRENCY', 'PHP'); 
+ 
+// Database configuration 
+define('DB_HOST', 'localhost'); 
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', ''); 
+define('DB_NAME', 'data-paypal'); 
+ 
+// Change not required 
+define('PAYPAL_URL', (PAYPAL_SANDBOX == true)?"https://www.sandbox.paypal.com/cgi-bin/webscr"
+:"https://www.paypal.com/cgi-bin/webscr");
